95bee48c0161664224c1e583ced000fde17bd3e1
Myk Melez <myk@mykzilla.org>
2016-06-06 08:56:30 -0700

    return correct URL after location change

--- positron/modules/atom_browser_web_contents.js
+++ positron/modules/atom_browser_web_contents.js
@@ -44,10 +44,7 @@ let WebContents_prototype = {
   getURL: function() {
     if (this.isGuest()) {
       if (this._webView) {
-        // TODO: return the correct URL after a goBack/goForward navigation,
-        // which doesn't update the 'src' attribute.  Probably we need to listen
-        // to locationchange events and keep track of the current URL ourselves.
-        return this._webView.browserPluginNode.getAttribute('src');
+        return this._webView.browserPluginNode.contentDocument.URL;
       }
       console.warn('cannot get URL for guest WebContents');
     } else {
